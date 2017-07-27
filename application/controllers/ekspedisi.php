163e068655bd6f75552f9210406dc4c8c658f99e
Tiyofirdhani <tiyofirdhani@gmail.com>
2017-07-27 10:50:54 +0700

    Fetch en

--- application/controllers/ekspedisi.php
+++ application/controllers/ekspedisi.php
@@ -6,6 +6,7 @@ class Ekspedisi extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('ekspedisimodel');
 	}
 
 	public function index()
---
+++ application/models/ekspedisimodel.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ekspedisimodel extends CI_Model {
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Do your magic here
+	}
+
+}
+
+/* End of file ekspedisimodel.php */
+/* Location: ./application/models/ekspedisimodel.php */
